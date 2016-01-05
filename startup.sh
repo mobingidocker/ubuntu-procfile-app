115af782f3149423155d2959e16c3f58fce601b4
David Siaw (via Travis CI) <david.siaw@mobingi.com>
2016-01-05 16:49:14 +0900

    streamline installation

--- startup.sh
+++ startup.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 echo "installing" > /var/log/container_status
 
-if [ ! -d "/app" ]; then
-	if [ -n "$GIT_REPO" ]; then
-		git clone "$GIT_REPO" /app
-		/build/builder
-	else
-		echo "No \$GIT_REPO environment variable defined"
-		exit 1
-	fi
-fi
+echo "Installing..."
+/build/builder
 
 if [ ! -f /app/Procfile ]; then
-    echo "Procfile is not found!"
+    echo "No Procfile found! Will attempt to run with default Procfile if there is any"
 fi
 
 echo "Running init script"
 bash /tmp/init/init.sh
 
-echo "complete" > /var/log/container_status
-
 HOME=/app source /app/.profile.d/*
 
-iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
+echo "Installation complete!"
+
+echo "complete" > /var/log/container_status
+
 /usr/bin/supervisord
