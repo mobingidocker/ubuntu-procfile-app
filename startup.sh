9e644330fd4dfdc2991ecf1af2b9678da77f1776
David Siaw (via Travis CI) <david.siaw@mobingi.com>
2016-01-05 18:38:14 +0900

    add support for custom buildpacks

--- startup.sh
+++ startup.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 echo "installing" > /var/log/container_status
 
+echo "Running init script"
+bash /tmp/init/init.sh
+
 echo "Installing..."
 rm -rf /app
 cp -r /code /app
+
+
+if [[ $BUILDPACK ]]; then
+	echo "Custom buildpack $BUILDPACK specified."
+	echo "export BUILDPACK_URL=$BUILDPACK" > /app/.env
+fi
+
 /build/builder
 
 if [ ! -f /app/Procfile ]; then
     echo "No Procfile found! Will attempt to run with default Procfile if there is any"
 fi
 
-echo "Running init script"
-bash /tmp/init/init.sh
+mkdir -p /app/.profile.d
+
+if [[ $BUILDPACK ]] && [ -f /app/.release ]; then
+	# in case its an old buildpack, we need to read .release
+	ruby -e 'require "yaml"; YAML.load_file("/app/.release")["config_vars"].each {|k,v| if k == "PATH"; puts "export #{k}=$#{k}:#{v}"; else; puts "export #{k}=\"#{v}\""; end }' > /app/.profile.d/mocloud_profile.sh 
+fi
 
 HOME=/app source /app/.profile.d/*
 
